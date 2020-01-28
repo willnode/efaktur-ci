ac5e4898827e4761a95217d4567d32e53f3e86a4
Wildan M <willnode@wellosoft.net>
2020-01-28 10:59:05 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -12,63 +12,49 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for cmstoolkit
-CREATE DATABASE IF NOT EXISTS `cmstoolkit` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `cmstoolkit`;
+-- Dumping database structure for dbefaktur
+CREATE DATABASE IF NOT EXISTS `dbefaktur` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `dbefaktur`;
 
--- Dumping structure for table cmstoolkit.admin
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
-  `id_login` int(11) NOT NULL,
-  `name_admin` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_admin`),
-  KEY `FK_admin_login` (`id_login`),
-  CONSTRAINT `FK_admin_login` FOREIGN KEY (`id_login`) REFERENCES `login` (`id_login`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+-- Dumping structure for table dbefaktur.dokumen
+DROP TABLE IF EXISTS `dokumen`;
+CREATE TABLE IF NOT EXISTS `dokumen` (
+  `dokumen_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dokumen_nama` varchar(255) NOT NULL DEFAULT '',
+  `dokumen_file` varchar(255) NOT NULL,
+  `dokumen_tgl` datetime NOT NULL DEFAULT curtime(),
+  `login_id` int(11) NOT NULL,
+  PRIMARY KEY (`dokumen_id`),
+  KEY `FK_dokumen_login` (`login_id`),
+  CONSTRAINT `FK_dokumen_login` FOREIGN KEY (`login_id`) REFERENCES `login` (`login_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table cmstoolkit.admin: ~0 rows (approximately)
-DELETE FROM `admin`;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` (`id_admin`, `id_login`, `name_admin`) VALUES
-	(1, 1, 'Admin');
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+-- Dumping data for table dbefaktur.dokumen: ~0 rows (approximately)
+DELETE FROM `dokumen`;
+/*!40000 ALTER TABLE `dokumen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dokumen` ENABLE KEYS */;
 
--- Dumping structure for table cmstoolkit.login
+-- Dumping structure for table dbefaktur.login
+DROP TABLE IF EXISTS `login`;
 CREATE TABLE IF NOT EXISTS `login` (
-  `id_login` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
-  `password` char(60) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `hp` varchar(255) NOT NULL DEFAULT '',
+  `avatar` varchar(255) NOT NULL DEFAULT '',
   `role` enum('admin','user') NOT NULL DEFAULT 'user',
-  PRIMARY KEY (`id_login`),
+  PRIMARY KEY (`login_id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table cmstoolkit.login: ~2 rows (approximately)
+-- Dumping data for table dbefaktur.login: ~1 rows (approximately)
 DELETE FROM `login`;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` (`id_login`, `username`, `password`, `role`) VALUES
-	(1, 'admin', '$2y$10$wVerUNh0IGO0QOkKyrSMTeicyfhTC/TRtEfTwFKPlPoCHIw9VHkf.', 'admin'),
-	(2, 'user', '$2y$10$gq7my7kh9wj/6ewnEKAkDeqTMhR5EvJO9jpTJZ75zjZ945sIEUcpC', 'user');
+INSERT INTO `login` (`login_id`, `username`, `password`, `name`, `hp`, `avatar`, `role`) VALUES
+	(1, 'admin', '$2y$10$spEDxL86Ni.uJUBFIql6be/7/R5f2kGDndZzk2OSrfJeI5SNU8KH2', 'Admin', '', '', 'admin'),
+	(2, 'user', '$2y$10$lnBjXDJE0c9WYqR8YoyMPeD4xaCfm.nEXr.E7SDU8LMmZPp14T3aa', 'User', '', '', 'user');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
-
--- Dumping structure for table cmstoolkit.user
-CREATE TABLE IF NOT EXISTS `user` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `id_login` int(11) NOT NULL,
-  `name_user` varchar(255) NOT NULL DEFAULT '',
-  `email_user` varchar(255) NOT NULL DEFAULT '',
-  `avatar_user` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_user`),
-  KEY `FK_user_login` (`id_login`),
-  CONSTRAINT `FK_user_login` FOREIGN KEY (`id_login`) REFERENCES `login` (`id_login`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- Dumping data for table cmstoolkit.user: ~1 rows (approximately)
-DELETE FROM `user`;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id_user`, `id_login`, `name_user`, `email_user`, `avatar_user`) VALUES
-	(1, 2, 'User', 'user@example.com', '');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
